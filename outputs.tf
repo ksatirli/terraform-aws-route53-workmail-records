df22bb792a4f63e0dc55c9857555c3e483a69069
Kerim Satirli <ksatirli@users.noreply.github.com>
2019-08-22 12:40:19 +0200

    Registry improvements (#2)
    * removes hook for `terraform validate` and adds descriptions to outputs
    
    * updates README.md with better provisioning instructions

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,54 @@
 output "ownership" {
-  value = aws_route53_record.ownership.name
+  value       = aws_route53_record.ownership.name
+  description = "interpolated value of `aws_route53_record.ownership.name`"
 }
 
 output "mx" {
-  value = aws_route53_record.mx.name
+  value       = aws_route53_record.mx.name
+  description = "interpolated value of `aws_route53_record.mx.name`"
 }
 
 output "autodiscover" {
-  value = aws_route53_record.autodiscover.name
+  value       = aws_route53_record.autodiscover.name
+  description = "interpolated value of `aws_route53_record.autodiscover.name`"
 }
 
 output "dkim-1" {
-  value = aws_route53_record.dkim-1.name
+  value       = aws_route53_record.dkim-1.name
+  description = "interpolated value of `aws_route53_record.dkim-1.name`"
 }
 
 output "dkim-2" {
-  value = aws_route53_record.dkim-2.name
+  value       = aws_route53_record.dkim-2.name
+  description = "interpolated value of `aws_route53_record.dkim-2.name`"
 }
 
 output "dkim-3" {
-  value = aws_route53_record.dkim-3.name
+  value       = aws_route53_record.dkim-3.name
+  description = "interpolated value of `aws_route53_record.dkim-3.name`"
 }
 
 output "spf" {
-  value = aws_route53_record.spf.name
+  value       = aws_route53_record.spf.name
+  description = "interpolated value of `aws_route53_record.spf.name`"
 }
 
 output "dmarc" {
-  value = aws_route53_record.dmarc.name
+  value       = aws_route53_record.dmarc.name
+  description = "interpolated value of `aws_route53_record.dmarc.name`"
 }
 
 output "mx_record" {
-  value = local.mx_record
+  value       = local.mx_record
+  description = "interpolated value of `local.mx_record`"
 }
 
 output "autodiscover_record" {
-  value = local.autodiscover_record
+  value       = local.autodiscover_record
+  description = "interpolated value of `local.autodiscover_record`"
 }
 
 output "zone_name" {
-  value = local.zone_name
+  value       = local.zone_name
+  description = "interpolated value of `local.zone_name`"
 }
