7efa23f8aa11e805f629d2d8ba38d3996f48e1e7
Kerim Satirli <k.satirli@gmail.com>
2020-01-22 14:51:47 +0100

    simplify

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "ownership" {
-  value       = aws_route53_record.ownership.name
-  description = "interpolated value of `aws_route53_record.ownership.name`"
-}
-
 output "mx" {
   value       = aws_route53_record.mx.name
   description = "interpolated value of `aws_route53_record.mx.name`"
@@ -13,19 +8,9 @@ output "autodiscover" {
   description = "interpolated value of `aws_route53_record.autodiscover.name`"
 }
 
-output "dkim-1" {
-  value       = aws_route53_record.dkim-1.name
-  description = "interpolated value of `aws_route53_record.dkim-1.name`"
-}
-
-output "dkim-2" {
-  value       = aws_route53_record.dkim-2.name
-  description = "interpolated value of `aws_route53_record.dkim-2.name`"
-}
-
-output "dkim-3" {
-  value       = aws_route53_record.dkim-3.name
-  description = "interpolated value of `aws_route53_record.dkim-3.name`"
+output "dkim" {
+  value       = aws_ses_domain_dkim.dkim.dkim_tokens
+  description = "interpolated value of `aws_ses_domain_dkim.dkim.dkim_tokens`"
 }
 
 output "zone_apex_txt_record" {
