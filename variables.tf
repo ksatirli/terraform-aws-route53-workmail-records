9c24c39890eae1168060f4046959683ed31be40d
Kerim Satirli <k.satirli@gmail.com>
2020-06-15 12:53:32 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -46,6 +46,6 @@ data "aws_route53_zone" "zone" {
 locals {
   mx_record            = "${var.mx_priority} inbound-smtp.${var.workmail_zone}.amazonaws.com."
   autodiscover_record  = "autodiscover.mail.${var.workmail_zone}.awsapps.com."
-  zone_apex_txt_record = "${var.spf_record} ${var.apex_txt_record_append}"
+  zone_apex_txt_record = "\"${var.spf_record}\" ${var.apex_txt_record_append}"
   zone_name            = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
 }
