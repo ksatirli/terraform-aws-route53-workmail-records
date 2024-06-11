699ca1f004b9a39344713b3bf67d984df6c2b599
Kerim Satirli <k.satirli@gmail.com>
2024-06-11 12:32:00 +0200

    linting

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/route53_zone
+data "aws_route53_zone" "zone" {
+  zone_id = var.zone_id
+}
+
+locals {
+  autodiscover_record  = "autodiscover.mail.${var.workmail_zone}.awsapps.com."
+  mx_record            = "${var.mx_priority} inbound-smtp.${var.workmail_zone}.amazonaws.com."
+  zone_apex_txt_record = concat(var.spf_record, var.apex_txt_record_append)
+  zone_name            = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
+}
+
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 resource "aws_route53_record" "mx" {
   zone_id = var.zone_id
@@ -26,6 +38,12 @@ resource "aws_route53_record" "autodiscover" {
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ses_domain_identity
 resource "aws_ses_domain_identity" "main" {
   domain = local.zone_name
+}
+
+# see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ses_domain_mail_from
+resource "aws_ses_domain_mail_from" "main" {
+  domain           = aws_ses_domain_identity.main.domain
+  mail_from_domain = "${var.mail_from_subdomain}.${aws_ses_domain_identity.main.domain}"
 }
 
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
--- variables.tf
+++ variables.tf
@@ -1,32 +1,3 @@
-variable "zone_id" {
-  type        = string
-  description = "ID of the DNS Zone to store Records in"
-}
-
-variable "record_ttl" {
-  type        = string
-  description = "TTL for all DNS records"
-  default     = 86400
-}
-
-variable "workmail_zone" {
-  type        = string
-  description = "AWS Zone of the WorkMail Organization"
-  default     = "us-east-1"
-}
-
-variable "mx_priority" {
-  type        = string
-  description = "MX Priority"
-  default     = 10
-}
-
-variable "spf_record" {
-  type        = list(string)
-  description = "SPF TXT Record"
-  default     = ["v=spf1 include:amazonses.com ~all;"]
-}
-
 variable "apex_txt_record_append" {
   type        = list(string)
   description = "additional Domain Apex TXT Record data"
@@ -39,13 +10,37 @@ variable "dmarc_record" {
   default     = "v=DMARC1;p=quarantine;pct=100;fo=1;"
 }
 
-data "aws_route53_zone" "zone" {
-  zone_id = var.zone_id
+variable "mail_from_subdomain" {
+  type        = string
+  description = "Subdomain to use as `MAIL FROM` domain."
+  default     = "mail"
 }
 
-locals {
-  mx_record            = "${var.mx_priority} inbound-smtp.${var.workmail_zone}.amazonaws.com."
-  autodiscover_record  = "autodiscover.mail.${var.workmail_zone}.awsapps.com."
-  zone_apex_txt_record = concat(var.spf_record, var.apex_txt_record_append)
-  zone_name            = data.aws_route53_zone.zone.name // NOTE: trailing period is added by data source
+variable "mx_priority" {
+  type        = string
+  description = "MX Priority"
+  default     = 10
+}
+
+variable "record_ttl" {
+  type        = string
+  description = "TTL for all DNS records"
+  default     = 86400
+}
+
+variable "spf_record" {
+  type        = list(string)
+  description = "SPF TXT Record"
+  default     = ["v=spf1 include:amazonses.com ~all;"]
+}
+
+variable "workmail_zone" {
+  type        = string
+  description = "AWS Zone of the WorkMail Organization"
+  default     = "us-east-1"
+}
+
+variable "zone_id" {
+  type        = string
+  description = "ID of the DNS Zone to store Records in"
 }
